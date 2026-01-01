1995da1f3ba9a8944dd346b0ac0891b8f860cbda
TienQTo <215321412+TienQTo@users.noreply.github.com>
2026-01-01 15:04:50 +0700

    Update app content and fix base path for new repository

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/classical-chinese-novel/',
+      base: '/translateChinesev1.0.0.1/',
       server: {
         port: 3000,
         host: '0.0.0.0',
